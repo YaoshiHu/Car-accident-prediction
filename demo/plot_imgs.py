2dc429048ba5cba7c6222f7aead5f902e6c2d2f9
YsHuStone <yaoshihu@outlook.com>
2018-12-11 11:02:42 -0500

    bolder line plot

--- demo/plot_imgs.py
+++ demo/plot_imgs.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from tqdm import tqdm
 
 score_data = np.load("data/prediction.npz")
 predict_scores = score_data["data"]
@@ -9,18 +10,22 @@ save_dir = "data/scores"
 
 def save_curve(video_id, num_img):
     save_file_name = os.path.join(save_dir, str(video_id), "000{}.jpg".format(str(num_img))[-8:])
-    print(save_file_name)
     score = predict_scores[video_id-1]
     figure = plt.figure(0)
     figure.clear()
     plt.xlim((1, 90))
     plt.ylim((0, 1))
-
-    plt.plot(range(1, num_img+1), score[0:num_img])
+    plt.title("Accident Probability Plot")
+    plt.xlabel("Frame in the video")
+    plt.ylabel("Score")
+    plt.plot(range(1, num_img+1), score[0:num_img], "r-", linewidth=4, label="score")
+    plt.legend()
     figure.savefig(save_file_name)
 
 
 if __name__ == "__main__":
     for clip in range(1, 11):
-        for img_id in range(0, 90):
+        print("Starting {} clip".format(clip))
+        for img_id in tqdm(range(0, 90)):
             save_curve(clip, img_id)
+        print("Finish {} clip".format(clip))
